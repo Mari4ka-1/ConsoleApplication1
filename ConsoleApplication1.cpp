94cbafaa75bbcc783e5cfa58416a44446fe9bc4d
Mari4ka-1 <krivoruchko.ms@phystech.edu>
2021-02-10 22:53:26 +0300

    new1

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << "bye world \n";
+    std::cout << "end world2 \n";
 }
  
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
